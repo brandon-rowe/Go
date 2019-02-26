d9be5262ef662f503118b2dba185921029aede27
BrandonRoweSC <[browe47@gmail.com]>
2019-02-26 18:02:17 -0500

    1 of 3 functions

---
+++ arithmetic.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("1 + 1 =", 1+1)
+    fmt.Println("1 + 1 =", 1.0+1.0)
+    fmt.Println("-1 + -1 =", (-1)+(-1))
+    fmt.Println("1 - 2 =", 1-2)
+    fmt.Println("10 x 24502 =", 10*24502)
+    fmt.Println("1.349 x 1.49 =", 1.349*1.49)
+    fmt.Println("1000 / 10 =", 1000/10)
+    fmt.Println("1000 % 3 =", 1000%3)
+    fmt.Println("1000 / 10 =", 1000/10)
+}
