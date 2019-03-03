ec74c8af2458bfee6793f4145b612946bc456bff
brandon-rowe <browe47@gmail.com>
2019-03-03 17:36:06 -0500

    update

--- intro/golang-book/Ch6/students.go
+++ intro/golang-book/Ch6/students.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Print("Please enter the number of students to enter: ")
-	var x float32
-	fmt.Scanf("%f", &x)
+	var x int
+	fmt.Scanf("%i", &x)
 	var name [x]string
-	for i:=0; i<(x); i++
-	{
+	for i:=0; i<x; i++{
 		fmt.Printf("Please enter the first name")
 		var tmp string
-		name[i] = fmt.("%s", &tmp)
+		fmt.("%s\n", &tmp)
+		name[i] = tmp
 	}
 }
