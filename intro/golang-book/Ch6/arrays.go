26ee3c17c3bee2cb18f10393b9e4c3b3cf4b948f
brandon-rowe <brandon@cyntax.org>
2019-03-03 19:57:50 -0500

    add extra

--- intro/golang-book/Ch6/arrays.go
+++ intro/golang-book/Ch6/arrays.go
@@ -14,4 +14,20 @@ func main(){
   fmt.Println(x)
   x[4] = 100
   fmt.Println(x)
+  array()
+}
+
+func array(){
+  var x [5]float64
+  x[0] = 99
+  x[1] = 93
+  x[2] = 77
+  x[3] = 82
+  x[4] = 83
+
+  var total float64 = 0
+  for i:=0; i<5; i++ {
+    total += x[i]
+  }
+  fmt.Println("Your average is ",total/5)
 }
