14286489b8fc2dcb6404f0626899aaf64169167e
brandon-rowe <brandon@cyntax.org>
2019-03-03 22:35:25 -0500

    saving work

--- intro/golang-book/Ch6/selection.go
+++ intro/golang-book/Ch6/selection.go
@@ -6,12 +6,17 @@ func main() {
 	var hi int = 0;
 	var lo int = 0;
 	x := []int{48,96,86,68,57,82,63,70,37,34,83,27,19,97,9,17}
-	for i, v := range x{
-		if v > hi
-			hi = v
-		if v < lo
-			lo = v
+	for index, value := range x {
+		if value > hi
+			hi = value
+		if value < lo
+			lo = value
 	}
 	fmt.Println(hi)
 	fmt.Println(lo)
 }
+
+
+//Current error message
+//./selection.go:11:14: syntax error: assignment hi = value used as value
+//./selection.go:13:14: syntax error: assignment lo = value used as value
