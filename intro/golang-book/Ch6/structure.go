2b700b48cb2229a4386cbd6ef58e82e642769699
brandon-rowe <brandon@cyntax.org>
2019-03-03 20:48:53 -0500

    edit structure.go

--- intro/golang-book/Ch6/structure.go
+++ intro/golang-book/Ch6/structure.go
@@ -20,24 +20,16 @@ func main() {
 	var x int
 	fmt.Scanln(&x)
 	fname := make([]string, x)
+	lname := make([]string, x)
 	for i:=0; i<x; i++{
 		fmt.Print("Please enter the first name of student ",i+1,": " )
 		var tmp string
 		fmt.Scanln(&tmp)
 		fname[i] = tmp
-	}
-
-	lname := make([]string, x)
-  	for i:=0; i<x; i++{
-	    fmt.Print("Please enter the last name of student ",i+1,": " )
-	    var tmp string
+		fmt.Print("Please enter the last name of student ",i+1,": " )
 	    fmt.Scanln(&tmp)
 	    lname[i] = tmp
 	}
-	fmt.Println("Students' first names are: ")
-	fmt.Println(fname)
-	fmt.Println("Students' last names are: ")
-  	fmt.Println(lname)
   	fmt.Println()
 
   	for i:=0; i<x; i++{
