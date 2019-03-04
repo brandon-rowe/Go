775b83b6fa86da653945eebab241a394b1df53f0
brandon-rowe <brandon@cyntax.org>
2019-03-04 10:58:55 -0500

    fixed selectionHiLo.go

--- intro/golang-book/Ch6/selectionHiLo.go
+++ intro/golang-book/Ch6/selectionHiLo.go
@@ -10,13 +10,13 @@ func main() {
 	x := []int{48,96,86,68,57,82,63,70,37,34,83,27,19,97,9,17}
 	for i, v := range x{
 		if i==0 {
-			hi := v
-			lo := v
-			break
+			hi = v
+			lo = v
 		}
-		
 	}
-	
+	//By using a loop and looking for index 0
+	//we can set the initial values of hi & lo
+	//After testing, and we're good.
 	for index, value := range x {
 		if value > hi{
 			hi = value
