6b7f2b1981ecf0c4d18daa3b96dcae908b1870be
brandon-rowe <brandon@cyntax.org>
2019-03-04 10:34:44 -0500

    selectionHi.go

--- intro/golang-book/Ch6/selectionHi.go
+++ intro/golang-book/Ch6/selectionHi.go
@@ -4,19 +4,14 @@ import "fmt"
 
 func main() {
 	var hi int = 0;
-	var lo int = 0;
 	x := []int{48,96,86,68,57,82,63,70,37,34,83,27,19,97,9,17}
 	for index, value := range x {
 		if value > hi{
 			hi = value
 		}
-		if value < lo{
-			lo = value
-		}
 		index++
 	}
 	fmt.Println(hi)
-	fmt.Println(lo)
 }
 
 
