e0da02108a238e9c4a52554dd14854aa5a4419d8
brandon-rowe <brandon@cyntax.org>
2019-03-18 21:37:54 -0400

    average function

--- intro/golang-book/Ch7/functions.go
+++ intro/golang-book/Ch7/functions.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func average(xs []float64) float64 {
-  panic("Not Implemented")
-}
-
-func main() {
-	xs := []float64{98,93,77,82,83}
-
 	total := 0.0
 	for _, v := range xs {
 		total += v
 	}
-	fmt.Println(total/float64(len(xs)))
-	fmt.Println(average(xs))
+	return total / float64(len(xs))
+}
 
+func main() {
+	xs := []float64{98,93,77,82,83}
+	fmt.Println(average(xs))
 }
